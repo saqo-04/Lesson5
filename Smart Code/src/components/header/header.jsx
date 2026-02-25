8006585c825c8cc441784e4915181b15c0c21e95
saqo-04 <sargisgrigoryan200@gmail.com>
2026-02-25 18:11:07 +0400

    Header

---
+++ Smart Code/src/components/header/header.jsx
@@ -0,0 +1,14 @@
+export function Header() {
+    return (
+      <header>
+        <img src="./assets/images/logo.png" alt="logo" />
+        <ul>
+          <li>Դասընթացներ</li>
+          <li>Առավելություններ</li>
+          <li>Թրեյներներ</li>
+          <li>Մեր մասին</li>
+        </ul>
+            <button type="button">Սկսել ուսումը</button>
+      </header>
+    );
+}
--- Smart Code/src/main.jsx
+++ Smart Code/src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Header } from './components/header/header.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <App />,
+    <Header />,
 )
